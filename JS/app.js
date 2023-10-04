c1b32ab435c4bc7ada1c8e9178b0f47f45655a9e
AnatoliyZakhryapin <anatoliy.zakhryapin@gmail.com>
2023-10-04 15:39:44 +0200

    ADD distanzeTrip

---
+++ JS/app.js
@@ -0,0 +1,33 @@
+// # Calcolo del prezzo del biglietto del treno
+
+// - Richiesta della distanza del percorso
+//     - Creare la variabile "distanceTrip"
+let distanceTrip;
+//     - Assegnare il valore tramite prompt
+distanceTrip = prompt ("Inserisci la distanza del viaggio");
+console.log (typeof distanceTrip, distanceTrip);
+//     - Trasformare il valore dalla stringa in numero con decimali (evitando anche Erorre del inserimento con la virgola)
+distanceTrip = parseFloat(distanceTrip.replace(/,/g, "."));
+console.log (typeof distanceTrip, distanceTrip);
+// - Richiesta dell'età 
+//     - Creare la variabile "passengerAge" 
+//     - Assegnare il valore tramite prompt
+// - Calcolo del prezzo totale del viaggio
+//     - Creare la variabile "priceKM" con valore 0.21 (valore euro per km)
+//     - Creare la variabile "discountUnder18" 20% in decimali
+//     - Creare la variabile "discountOver65" 40% in decimali
+//     - Creare la variabile "discountNot" 0% in decimali
+//     - Creare la variabile "priceBasic"
+//     - Calcolare il prezzo base 
+//         priceBasic = distanceTrip * priceKm
+//     - Creare la variabile "passengerDiscountEntity"
+//         SE (passengerAge < 18) 
+//             - passengerDiscountEntity = discountUnder18
+//         ALTRIMENTI SE (passengerAge > 65) 
+//             - passengerDiscountEntity = discountUnder65
+//         ALTRIMENTI () 
+//             - passengerDiscountEntity = discountNot
+//     - Creare la variabile "dicountPassenger"
+//         - discountPassenger = priceBasic * passengerDiscountEntity / 100
+//     - Creare la variabile "totalPrice"
+//         - totalPrice = priceBasic - discountPassenger
