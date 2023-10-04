a3831c267d44e1d7d5c50a7642cde22155c7de57
AnatoliyZakhryapin <anatoliy.zakhryapin@gmail.com>
2023-10-04 15:44:38 +0200

    ADD passengerAge

--- JS/app.js
+++ JS/app.js
@@ -4,14 +4,20 @@
 //     - Creare la variabile "distanceTrip"
 let distanceTrip;
 //     - Assegnare il valore tramite prompt
-distanceTrip = prompt ("Inserisci la distanza del viaggio");
+distanceTrip = prompt("Inserisci la distanza del viaggio");
 console.log (typeof distanceTrip, distanceTrip);
 //     - Trasformare il valore dalla stringa in numero con decimali (evitando anche Erorre del inserimento con la virgola)
 distanceTrip = parseFloat(distanceTrip.replace(/,/g, "."));
 console.log (typeof distanceTrip, distanceTrip);
 // - Richiesta dell'età 
-//     - Creare la variabile "passengerAge" 
+//     - Creare la variabile "passengerAge"
+let passengerAge;
 //     - Assegnare il valore tramite prompt
+passengerAge = prompt("Inserisci l'età");
+console.log(typeof passengerAge, passengerAge);
+//     - Trasformare il valore dalla stringa in numero con decimali
+passengerAge = parseInt(passengerAge);
+console.log(typeof passengerAge, passengerAge);
 // - Calcolo del prezzo totale del viaggio
 //     - Creare la variabile "priceKM" con valore 0.21 (valore euro per km)
 //     - Creare la variabile "discountUnder18" 20% in decimali
